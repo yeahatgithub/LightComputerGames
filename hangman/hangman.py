dc9b99c67e03e3026a8abf0c4594b8d070757651
freedomyeah <chchye@nudt.edu.cn>
2018-06-15 10:29:35 +0800

    猜固定单词，列出没猜中的字母 1. 列出没猜中的字母序列。 2. 修改了部分提示，程序运行期间给用户的提示，的文字表达

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -59,28 +59,32 @@ HANGMAN_LIST = [
         ]
 
 guess_error = 0
-guessed_letter = ['_', '_', '_']
+guessed_letters = ['_', '_', '_']
+missed_letters = ''
 while guess_error < len(HANGMAN_LIST) - 1:
     print(HANGMAN_LIST[guess_error])
-    print("缺失的字母：")
-    print(' '.join(guessed_letter))
-    g = input("输入下一个字母：")
+    print("当前空缺：")
+    print(' '.join(guessed_letters))
+    if len(missed_letters) > 0:
+        print("没猜中的字母：" + missed_letters)
+    g = input("\n你猜的下一个字母是：")
     hit = False
     for i in range(len(target_word)):
         if target_word[i] == g:
-            guessed_letter[i] = g
+            guessed_letters[i] = g
             hit = True
     if hit:
-        if '_' not in guessed_letter:
+        if '_' not in guessed_letters:
             break
     else:
         guess_error += 1
+        missed_letters += g
 
 if guess_error == len(HANGMAN_LIST) - 1:
     print("真不幸，你丢命了！")
     print(HANGMAN_LIST[guess_error])
 else:
-    print("你猜对了！")
+    print("你猜对了！被猜测的单词是：" + target_word)
     print("棒棒哒！")
 
 
