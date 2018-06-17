61a3d9c705e16acbead257f304d78d769de8657f
freedomyeah <chchye@nudt.edu.cn>
2018-06-17 14:06:04 +0800

    三连棋游戏，绘制棋盘 1. 绘制“#”，棋盘有9个格子。

--- san-lian-qi/main.py
+++ san-lian-qi/main.py
@@ -4,6 +4,12 @@
 # @Copyright:  MIT
 import pygame
 WHITE = (255, 255, 255)
+LINE_COLOR = (0, 255, 0)
+LINE_WIDTH = 5
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+BOARD_SIZE = 300
+
 
 def main():
     pygame.init()
@@ -17,8 +23,21 @@ def main():
                 is_playing = False
 
         screen.fill(WHITE)
+        draw_board(screen)
         pygame.display.update()
 
+def draw_board(screen):
+    for r in range(1, 3):
+        left_top = ( (WINDOW_WIDTH - BOARD_SIZE) // 2, (WINDOW_HEIGHT - BOARD_SIZE)//2 + r * BOARD_SIZE // 3 )
+        w_h = (BOARD_SIZE, 5)
+        line = pygame.Rect(left_top, w_h)
+        pygame.draw.rect(screen, LINE_COLOR, line)
+
+    for c in range(1, 3):
+        left_top = ( (WINDOW_WIDTH - BOARD_SIZE) // 2 + c * BOARD_SIZE // 3, (WINDOW_HEIGHT - BOARD_SIZE)//2 )
+        w_h = (5, BOARD_SIZE)
+        line = pygame.Rect(left_top, w_h)
+        pygame.draw.rect(screen, LINE_COLOR, line)
 
 if __name__ == "__main__":
     main()
